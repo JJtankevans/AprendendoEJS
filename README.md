3bf3df832b2d30e72feb1a89a7c3dcb244a5c9e5
JJtankevans <v.oliveira042997@gmail.com>
2022-09-29 12:19:32 -0400

    Finalização do projeto após as etapas de criar a pasta partials e o recebimento de dados do 'backend'

---
+++ README.md
@@ -0,0 +1,9 @@
+# Aprendendo NodeJS com EJS
+-----------------------------------------------
+
+- Olá, bem vindo eu me chamo Vitor e neste projeto simples estou aprendendo a usar NodeJS juntamente com o EJS
+
+- Para executar o projeto basta clonar o repositório do github em seguida executar o comando "npm install" para que os pacotes do EJS e do Express possam ser instalados 
+
+- Em seguida execute o comando "node server" no seu terminal 
+- Abra seu navegador e entre em "localhost:8080" e pronto o projeto estará rodando no seu computador
--- server.js
+++ server.js
@@ -7,11 +7,44 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    res.render("index");
+    const items = [
+        { 
+            title: 'D',
+            message: "esenvolver aplicações/serviços de forma fácil"
+        },
+        { 
+            title: 'E',
+            message: "JS usa JavaScript para renderizar HTML"
+        },
+        { 
+            title: 'M',
+            message: "uito fácil de usar"
+        },
+        { 
+            title: 'A',
+            message: "morzinho"
+        },
+        { 
+            title: 'I',
+            message: "nstall ejs"
+        },
+        { 
+            title: 'S',
+            message: "intaxe simples"
+        }
+
+    ];
+    
+    const subtitle = "Uma linguagem de modelagem para criação de página HTMl usando JavaScript"
+
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle
+    });
 })
 
 app.get("/sobre", function (req, res) {
-    res.render("about");
+    res.render("pages/about");
 })
 
 app.listen(8080);
